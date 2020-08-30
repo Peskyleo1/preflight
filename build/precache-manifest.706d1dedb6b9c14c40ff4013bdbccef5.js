a25b30147f085fbc9db771752c2ff1cd87711db4
Leonardo <peskyleo1@gmail.com>
2020-08-30 16:23:26 +0200

    build

---
+++ build/precache-manifest.706d1dedb6b9c14c40ff4013bdbccef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac0e996a233f9518833e64584c1b7f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7072a41d252ebc87bcb4",
+    "url": "/static/css/main.0607bb6c.chunk.css"
+  },
+  {
+    "revision": "2e58f55a3f2deff49638",
+    "url": "/static/js/2.d2a2121d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2a2121d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7072a41d252ebc87bcb4",
+    "url": "/static/js/main.74de3f2c.chunk.js"
+  },
+  {
+    "revision": "e69829f6d321f060b14c",
+    "url": "/static/js/runtime-main.b9fa9e75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
